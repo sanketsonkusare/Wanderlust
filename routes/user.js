70614910a19375b5bf6b79fd1b8e9e18567d7379
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-01-03 23:02:05 +0530

    🎉 feat: add login route and view, implement authentication with passport

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const wrapAsync = require("../utils/wrapAsync");
 const router = express.Router();
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -19,5 +20,16 @@ router.post("/signup", wrapAsync(async (req, res) => {
         res.redirect("/signup");
     }
 }));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login",   passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), async (req, res) =>{
+    res.flash("success","Welcome user");
+    res.redirect("/listings");
+});
+
+
 
 module.exports  = router;
