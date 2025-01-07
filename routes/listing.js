5262258fdcc2b0a4a3c615f62f9cd18c7d846289
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-01-07 13:26:52 +0530

    🎉 feat: update Cloudinary integration with new storage configuration, add .env to .gitignore, and refactor multer setup for image uploads

--- routes/listing.js
+++ routes/listing.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router.route("/")
     .get(wrapAsync( listingController.index))
